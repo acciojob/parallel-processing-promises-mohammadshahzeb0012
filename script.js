2f8a19385c08b8d50b51bf2796e2a78ea3cd1be2
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-04-02 23:02:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,47 +1,36 @@
 //your JS code here. If required.
-// const output = document.getElementById("output");
-// const btn = document.getElementById("download-images-button");
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
 
-// const images = [
-//   { url: "https://picsum.photos/id/237/200/300" },
-//   { url: "https://picsum.photos/id/238/200/300" },
-//   { url: "https://picsum.photos/id/239/200/300" },
-// ];
-        const output = document.getElementById("output");
-        const btn = document.getElementById("download-images-button");
+const images = [
+    { url: "https://picsum.photos/id/237/200/300" },
+    { url: "https://picsum.photos/id/238/200/300" },
+    { url: "https://picsum.photos/id/239/200/300" },
+];
 
-        const images = [
-            { url: "https://picsum.photos/id/239/200/300" },
-            { url: "https://fastly.picsum.photos/id/238/200/300.jpg?hmac=WF3u-tnO4aoQvz_F9p7zS0Dr5LwGx74tPabQf7EjHkw" },
-            { url: "https://picsum.photos/id/239/200/300" },
-        ];
+btn.addEventListener("click", function () {
+    let p1 = downloadImage(images[0].url);
+    let p2 = downloadImage(images[1].url);
+    let p3 = downloadImage(images[2].url);
 
-        // Function to download image from a given URL
-        const downloadImage = async (image) => {
-            return new Promise((resolve, reject) => {
-                const img = new Image();
-                img.onload = () => resolve(img);
-                img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
-                img.src = image.url;
-            });
-        };
+    let promiseArray = Promise.all([p1, p2, p3]);
 
-        // Function to download all images in parallel
-        const downloadImages = async (images) => {
-            try {
-                // Use Promise.all to download images in parallel
-                const downloadedImages = await Promise.all(images.map(image => downloadImage(image)));
+    promiseArray.then((img) => {
+        for (let i = 0; i < img.length; i++) {
+            output.appendChild(img[i]);
+        }
+    }).catch((err) => { console.log(err) });
+})
 
-                // Append each downloaded image to the output div
-                downloadedImages.forEach(image => {
-                    output.appendChild(image);
-                });
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        // Event listener to start downloading images on button click
-        btn.addEventListener('click', () => {
-            downloadImages(images);
-        });
+function downloadImage(url) {
+    let img = new Image();
+    return new Promise((resolve, reject) => {
+        img.onload = function () {
+            resolve(img);
+        }
+        img.onerror = function () {
+            reject(new Error(`Failed to load image's URL : ${url}`));
+		}
+        img.src = url;
+    })
+}
